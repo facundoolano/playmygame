7710a4c51ad9b37de56a9d48dbd85aa3edb06cf3
Facundo Olano <facundo.olano@gmail.com>
2015-04-09 11:14:25 -0300

    fetching app from database

--- public/javascripts/controllers/show.js
+++ public/javascripts/controllers/show.js
@@ -1,12 +1,40 @@
 var showAppCtrl = angular.module('showAppCtrl', []);
 
-showAppCtrl.controller('showAppCtrl', ['$scope', '$http', function($scope, $http) {
-	$http.get('/apps')
-		.success(function(data) {
-			$scope.app = data;
-		})
-		.error(function(data) {
-			console.log('Error getting app: ' + data);
-		});
+showAppCtrl.controller('showAppCtrl', ['$scope', '$filter', '$http', function($scope, $filter, $http) {
+    $scope.app = undefined;
+    $scope.description = description;
+    $scope.stars = stars;
+    $scope.next = next;
+
+    $http.get('/apps')
+        .success(function(data) {
+            $scope.app = data;
+        })
+        .error(function(data) {
+            console.log('Error getting app: ' + data);
+        });
+
+    function next() {
+        //TODO
+    }
+
+    function description() {
+        if (!$scope.app) return "";
+        var desc = $scope.app.description;
+
+        var LIMIT = 200;
+
+        if (desc.length > LIMIT) {
+            desc = $filter("limitTo")(desc, LIMIT - 1) + "…";
+        }
+
+        return desc;
+    }
+
+    function stars() {
+        if (!$scope.app) return 0;
+        var count = Math.round($scope.app.score);
+        return new Array(count);
+    }
 
 }]);
--- routes/apps.js
+++ routes/apps.js
@@ -2,11 +2,17 @@ var express = require("express");
 var router = express.Router();
 var gplay = require("google-play-scraper");
 var App = require("../models/app.js");
-var winston = require('winston');
+var winston = require("winston");
 
 /* GET an application. */
 router.get("/", function(req, res) {
-    res.send("respond with a resource");
+    //TODO get the one with better priority
+    App.count()
+        .then(function(count) {
+            var rand = Math.floor(Math.random() * count);
+            return App.findOne().skip(rand).lean().exec();
+        })
+        .then(res.json.bind(res));
 });
 
 /* POST an application. */
@@ -17,7 +23,7 @@ router.post("/", function(req, res) {
 
     //TODO handle wrong app id
     gplay(id)
-        .catch(function(){
+        .catch(function() {
             raise("The given Google Play ID is invalid.");
         })
         .then(validateData)
